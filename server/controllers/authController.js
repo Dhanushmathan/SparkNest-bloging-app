2196c3bd607ae6f5ed09cc21741ab0befc63a26c
Dhanushmathan <lovelymathan2394@gmail.com>
2025-05-16 12:44:58 +0530

    add a middlewares and a funtion to handle error

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,11 +1,12 @@
 import UserModel from "../model/userModel.js";
 import bcrypt from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
 
     if (!username || !email || !password || username === '' || email === '' || password === '') {
-        return res.status(400).json({ message: 'All fields are required' });
+        next(errorHandler(400, 'All fields are required'));
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -20,6 +21,6 @@ export const signup = async (req, res) => {
         await newUser.save();
         return res.status(200).json({ message: 'Signup successful' });
     } catch (error) {
-        return res.status(500).json({ message: 'Internal server error' });
+        next(error);
     }
 }
--- server/index.js
+++ server/index.js
@@ -13,6 +13,17 @@ connectDB();
 app.use('/api/users', userRouter);
 app.use('/api/auth', authRouter);
 
+// Middleware to handle 404 errors
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
+
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
