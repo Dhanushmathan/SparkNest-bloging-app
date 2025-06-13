9ff55d334f3187b0d4978a8a7f2a0580c4cb581e
Dhanushmathan <lovelymathan2394@gmail.com>
2025-06-13 16:17:22 +0530

    complete update user functionality

--- server/index.js
+++ server/index.js
@@ -4,19 +4,21 @@ import cors from 'cors';
 import { connectDB } from './config/config.js';
 import userRouter from './routes/userRoutes.js';
 import authRouter from './routes/authRoutes.js';
-import profileRouter from './routes/userRoutes.js';
+// import profileRouter from './routes/userRoutes.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 dotenv.config();
 connectDB();
 
 app.use('/api/users', userRouter);
 app.use('/api/auth', authRouter);
-app.use('/api/profile', profileRouter);
+// app.use('/api/profile', profileRouter);
 
 // Middleware to handle 404 errors
 app.use((err, req, res, next) => {
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { uploadProfileImage } from '../controllers/userControllers.js';
+import { updateUser, uploadProfileImage } from '../controllers/userControllers.js';
 import upload from '../middlewares/upload.js';
+import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post('/upload-profile/:userId', upload.single('image'), uploadProfileImage);
+router.put('/update-user/:userId', verifyToken, updateUser);
 
 export default router;
