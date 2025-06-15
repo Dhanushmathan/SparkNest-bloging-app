39a2f40f8dcba7af1a990238ba742dc3d79dfc8e
Dhanushmathan <lovelymathan2394@gmail.com>
2025-06-15 12:43:55 +0530

    add delete user api route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { updateUser, uploadProfileImage } from '../controllers/userControllers.js';
+import { deleteUser, updateUser, uploadProfileImage } from '../controllers/userControllers.js';
 import upload from '../middlewares/upload.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
@@ -7,5 +7,6 @@ const router = express.Router();
 
 router.post('/upload-profile/:userId', upload.single('image'), uploadProfileImage);
 router.put('/update-user/:userId', verifyToken, updateUser);
+router.delete('/delete-user/:userId', verifyToken, deleteUser);
 
 export default router;
